d4eb3051d07f4914e68f5c6ce8437c01e29d2d69
Aryaman Arora <aryaman.arora2020@gmail.com>
2018-09-17 22:04:22 -0400

    getQuestion feature, organize functions in main.py

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 from wiktionaryparser import WiktionaryParser
 from flask import Flask, request, json, Response
+import random
 
 parser = WiktionaryParser()
 app = Flask(__name__)
 
-def query():
-    data = parser.fetch(request.args.get('w'))
+website_name = 'http://whateverwecallourwebsite.com'
+
+def get_definition(word):
+    return parser.fetch(word)[0]['definitions'][0]['text'][1]
+
+def make_json_reponse(data):
     js = json.dumps(data)
     resp = Response(js, status=200, mimetype='application/json')
-    resp.headers['Link'] = 'http://whateverwecallourwebsite.com'
+    resp.headers['Link'] = website_name
     return resp
+
+@app.route('/api/getDefinition/<word>')
+def query(word):
+    data = {
+        'definition' : get_definition(word)
+    }
+    return make_json_reponse(data)
+
+@app.route('/api/getQuestion')
+def get_question():
+    fin = open("words.txt", "r")
+    all_words = fin.readlines()
+    words = []
+    while len(words) < 4:
+        word = random.choice(all_words).strip()
+        if word not in words:
+            words.append(word)
+    ans = random.randint(0, 4)
+    data = {
+        'options' : words,
+        'definition' : get_definition(words[ans]),
+        'answer' : ans
+    }
+    return make_json_reponse(data)
