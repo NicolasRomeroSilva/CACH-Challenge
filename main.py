d89cddd5f8ef9ccedaa45e1920dd447d1d2791ce
aryamanarora <aryaman.arora2020@gmail.com>
2018-09-22 00:25:46 -0400

    add ability to make multiple queries at once, fix getQuestion issues

--- main.py
+++ main.py
@@ -9,34 +9,57 @@ print(__name__)
 website_name = 'http://whateverwecallourwebsite.com'
 
 def get_definition(word):
-    return parser.fetch(word)[0]['definitions'][0]['text'][1]
 
-def make_json_reponse(data):
+    try:
+        return parser.fetch(word)[0]['definitions'][0]['text'][1]
+    except:
+        return ""
+
+def make_json_reponse(data, status):
+
     js = json.dumps(data)
-    resp = Response(js, status=200, mimetype='application/json')
+    resp = Response(js, status=status, mimetype='application/json')
     resp.headers['Link'] = website_name
     return resp
 
-@app.route('/api/getDefinition/<word>')
-def query(word):
-    data = {
-        'definition' : get_definition(word)
-    }
-    return make_json_reponse(data)
+@app.route('/api/getDefinition/<query>')
+def query(query):
+
+    words = query.split(',')
+    data = {}
+    error = False
+
+    for word in words:
+        data[word] = {'definition' : get_definition(word)}
+        if data[word]['definition'] == "":
+            print(word + " has an error")
+            error = True
+    
+    return make_json_reponse(data, 400 if error else 200)
 
 @app.route('/api/getQuestion')
 def get_question():
+    count = int(request.args.get('count'))
+    if count is None:
+        count = 1
+
     fin = open("words.txt", "r")
     all_words = fin.readlines()
-    words = []
-    while len(words) < 4:
-        word = random.choice(all_words).strip()
-        if word not in words:
-            words.append(word)
-    ans = random.randint(0, 4)
-    data = {
-        'options' : words,
-        'definition' : get_definition(words[ans]),
-        'answer' : ans
-    }
-    return make_json_reponse(data)
+
+    data = {}
+    for i in range(count):
+        words = []
+
+        while len(words) < 4:
+            word = random.choice(all_words).strip()
+            if word not in words:
+                words.append(word)
+        ans = random.randint(0, 3)
+
+        data.update({i : {
+            'options' : words,
+            'definition' : get_definition(words[ans]),
+            'answer' : ans
+        }})
+
+    return make_json_reponse(data, 200)
